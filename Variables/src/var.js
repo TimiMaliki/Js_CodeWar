4fad8199363b63a5d482225915354e4c4939fec9
user.name <malikitimi@gmail.com>
2022-07-12 18:16:29 +0100

    operand

---
+++ Variables/src/var.js
@@ -0,0 +1,21 @@
+// i'm currently starting my first variable and declaring it
+
+var money = 200;
+
+// now i'm learning the difference between  a local and global
+
+var myVar = "Global" // declares a global
+
+// now declaring a local variable
+ function checkscope(){
+    var myVar ="Local" // declare a Local
+    document.write(myVar)
+ };
+  // this section i'm learning operand
+   var A = 10;
+   B = 11;
+
+   sum = A + B;
+    console.log (sum);
+
+    
