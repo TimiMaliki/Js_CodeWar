fac0665f5f06dc29d8f1a84967bd1a5152716590
user.name <malikitimi@gmail.com>
2022-08-08 04:41:40 +0100

    function

---
+++ function/index.js
@@ -0,0 +1,22 @@
+function sumUp(first,last){
+    let age;
+    age = first + last;
+}
+
+function secondFunction(){
+    let name;
+     name = sumUp('timi','maliki');
+     console.log();
+}
+
+function myAge(name ,age){
+    let displayName;
+
+    displayName = name + " " + "is " + "" + "about to be " +  age +" "+ "old";
+    console.log(displayName)
+}
+myAge("timi" ,27)
+
+
+
+   
