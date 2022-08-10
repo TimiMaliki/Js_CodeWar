1652e986d23114f71b97a9f53f697e0bb58006af
user.name <malikitimi@gmail.com>
2022-08-10 02:57:19 +0100

    Event

--- Event/index.js
+++ Event/index.js
@@ -4,4 +4,10 @@
 function sayHello(){
     console.log('hello World')
     document.write('hello world')
-}
+};
+
+//the onSubmit Event type
+function yoo(){
+    console.log('yoo World')
+    document.write('yoo world')
+};
