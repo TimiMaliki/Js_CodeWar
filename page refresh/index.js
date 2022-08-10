6c71dcbaa0f700a269854cdc8ad6ecfc5392b9d9
user.name <malikitimi@gmail.com>
2022-08-10 23:55:40 +0100

    code-war

--- page refresh/index.js
+++ page refresh/index.js
@@ -1,12 +1,13 @@
 // this is section is about page refresh and redirection
 
-function AutoRefresh( t ){
-    setTimeout("location.reload(true);",t);
-};
-AutoRefresh(5000);
+// function AutoRefresh( t ){
+  //  setTimeout("location.reload(true);",t);
+//};
+//AutoRefresh(5000);
+//
 
-function newDirect(){
-    window.location="http://www.tutorialspoint.com";
-}
+function Redirect(){
+    window.location="http://netNaija.com";
+};
 
     
