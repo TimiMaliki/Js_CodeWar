7cb8a358e6f64e7a41a5ca083e672dbc83e0e9eb
user.name <malikitimi@gmail.com>
2022-08-11 15:02:11 +0100

    alert

--- alert/index.js
+++ alert/index.js
@@ -1,9 +1,29 @@
-function Message(){
-    alert('newMovies');
+/*function Message(){
+   
     window.location='http://netflix.com'
 }
 document.write('follow for more movies');
 setTimeout(Message(),10000);
+
+function warn(){
+    alert('tis site might be dangerus');
+}
+warn(Message());
+document.write('movies')
+*/
+function getConfrimed(){
+    let retVal = confirm('do you wish to continue')
+    if(retVal =! true)// but if == click yes
+    {
+       document.write('click yes');
+       return true;
+    }
+    else{
+      document.write('ignore');
+      return false;
+    }
+}
+getConfrimed();
 
 
 
