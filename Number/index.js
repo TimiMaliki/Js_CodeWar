62d1edd8501be648558626f11afa0cdf33093336
user.name <malikitimi@gmail.com>
2022-08-15 17:54:00 +0100

    code-war

--- Number/index.js
+++ Number/index.js
@@ -1,5 +1,13 @@
+//this section is for Numbers
 let val = new Number(2);
-console.log(val)
-
+console.log( typeof val)
+// MAX_Value this is the largest possible number object javascript can work
 let max = Number.MAX_VALUE;
-console.log(max)
+console.log( max)
+console.log( typeof max)
+//console.log("max of the number :"+ val)
+
+// Min_Value this is the smallest possible number object javascript can work
+let min = Number.MIN_VALUE;
+console.log(min)
+console.log( typeof min)
