242ce4a1a64b701b68ef7f753080efedac9c3eb9
user.name <malikitimi@gmail.com>
2022-08-16 04:31:03 +0100

    String

--- boolean/index.js
+++ boolean/index.js
@@ -1,8 +1,25 @@
 // this section is for boolean
 
 let val = new Boolean(true);
- console.log(val);
+// console.log(val);
 
  let bool = new Boolean("good boy");
 
- console.log("bool.constructor() is : " + bool.constructor);
+// console.log("bool.constructor() is : " + bool.constructor);
+ 
+ //the source method
+
+ function bookClub(title, author, price){
+    this.title = title;
+    this.author = author;
+    this.price = price;
+
+ };
+ 
+    
+    
+
+
+let newBook = new bookClub("New Born" , "Maliki" , "200");
+
+ console.log("newBook.toSource() is : "+ newBook.toSource())
