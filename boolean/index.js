50ff00d9350ad34bdc5cec45bd40c91fb5531f8e
user.name <malikitimi@gmail.com>
2022-08-20 18:50:45 +0100

    Array

--- boolean/index.js
+++ boolean/index.js
@@ -1,4 +1,9 @@
 // this section is for boolean ,true or false
  let iamHuman = true
- console.log(iamHuman)
+ console.log(iamHuman);
+ //using comparison
+ console.log(7 >= 8); //false
+ console.log(7 >= 7); //true
+ 
+
 
