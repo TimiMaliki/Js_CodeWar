25535831d4ba6f1f96fe486686847e000a9233dc
user.name <malikitimi@gmail.com>
2022-08-28 20:56:49 +0100

    code-war

--- Date/index.js
+++ Date/index.js
@@ -1,11 +1,24 @@
  /**
   * This section is for Date
-  * 
+  * you can add parameters to the Date()
+  * e.g (2022, 9(which is September), 3,43,43,23)
   */
 
- let myDate = new Date();
- console.log(myDate);
+ let date1 = new Date();
+ /***
+  * toDateString  help shows the date 
+  * while toTime String return the value to a string
+  * 
+  * toUTCString shows the time in gmt or of the browser */ 
+ console.log(date1.toDateString());
 
  //Date Methodd ---get.Date method
 
- 
+ /**
+  * THE get.Day method gets the date of the month
+  */
+let date2 = new Date("9/28/2022");
+console.log(date2.getDate());
+
+
+
