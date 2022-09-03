570701578d90d9ccd42a83f77e336d41d368664f
user.name <malikitimi@gmail.com>
2022-09-03 15:13:03 +0100

    code-war

--- Math/index.js
+++ Math/index.js
@@ -13,10 +13,41 @@ console.log(PI);
  let toDecimalPlace = PI.toFixed(2)
   console.log(toDecimalPlace)
 
+  //Math method
+
   /***
- * @param(Number) PI
+ * @param(Number) min
  * The  Math.min
+ * this gets the smallest or lowest value
  */
  
   let min = Math.min(0.5 , 1 ,2);
   console.log(min)
+
+   /***
+ * @param(Number) max
+ * The  Math.max
+ * this get the largest value
+ */
+    let max = Math.max(0.5 , 1 ,2);
+    console.log(max)
+
+
+      /***
+ * @param(Number) power
+ * The  Math.pow
+ * this method raise the value power
+ */
+ let power = Math.pow(2,3)
+ console.log(power)
+
+  
+      /***
+ * @param(Number) random
+ * The  Math.random
+ * this method generates random numbers
+ */
+ let random = Math.random()
+ console.log(random.toFixed(2))
+
+  
