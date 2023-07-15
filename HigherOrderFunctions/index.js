9b645df2f280bd45b18d81d94a0961bc2193f1d7
TimiMaliki <malikitimi@gmail.com>
2023-07-15 17:48:25 +0100

    filter

--- HigherOrderFunctions/index.js
+++ HigherOrderFunctions/index.js
@@ -3,13 +3,13 @@
 // let five = 5;
 
 let Books = [
-    {name:'way of a superior man' , Year: "1891"},
-    {name:'Law of attraction' , Year: "1792"  , favourites: 'favourite'} ,
-    {name:'warrior within' , Year: "1893"},
-    {name:'12 rules of life' , Year: "1794"},
-    {name:'Atomic Habits' , Year: "1895" , favourites: 'favourite'},
-    {name:'Power Of the mind' , Year: "1895"},
-    {name:'Power Of the God' , Year: "1895"},
+    {name:'way of a superior man' , Year: 2000 , soldYears:2030},
+    {name:'Law of attraction' , Year: 2000  , favourites: 'favourite' , soldYears:2010} ,
+    {name:'warrior within' , Year: 2000 , soldYears:2014},
+    {name:'12 rules of life' , Year: 2000 , soldYears:2001},
+    {name:'Atomic Habits' , Year: 2000 , favourites: 'favourite' , soldYears:2020},
+    {name:'Power Of the mind' , Year: 2000 , soldYears:2022} , 
+    {name:'Power Of the God' , Year: 2000 , soldYears:2023}, 
 
 ]
 
@@ -33,7 +33,7 @@ let scores = [10,20,30,40,50,60,70,80,90,100]
 Books.forEach(function(myBook){
 //   console.log(myBook.name);
 })
- // for each end
+ // for each soldYears
 
 
 
@@ -76,4 +76,9 @@ const myFavs = Books.filter(fav => fav.favourites === 'favourite')
 
 const seventies = Books.filter(sevens => (sevens.Year < 1800))
 
-console.log(seventies);
+// get if the sold years lasted 10 years
+
+const sold = Books.filter(sell => (sell.soldYears - sell.Year >= 30))
+
+
+console.log(sold);
