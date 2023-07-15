590412237d18e12c0cd1bcc80b9c2fcdbe97734a
TimiMaliki <malikitimi@gmail.com>
2023-07-15 16:44:23 +0100

    for each

---
+++ HigherOrderFunctions/index.js
@@ -0,0 +1,39 @@
+//  testing
+
+// let five = 5;
+
+let Books = [
+    {name:'way of a superior man' , Year: "1891"},
+    {name:'Law of attraction' , Year: "1892"},
+    {name:'warrior within' , Year: "1893"},
+    {name:'12 rules of life' , Year: "1894"},
+    {name:'Atomic Habits' , Year: "1895"},
+    {name:'Power Of the mind' , Year: "1895"},
+    {name:'Power Of the God' , Year: "1895"},
+
+]
+
+let scores = [10,20,30,40,50,60,70,80,90,100]
+//  console.log(Books.length);
+
+// for
+// for(let i = 0 ; i < Books.length ; i++){
+//    document.write(i)
+  
+// }
+
+
+
+
+
+// for each
+
+Books.forEach(function(myBook){
+  console.log(myBook.name);
+})
+
+
+//filter
+
+
+
