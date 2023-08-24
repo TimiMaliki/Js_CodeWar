3fc4c78f974987be3fdf5f3b795d410375ab6fc6
TimiMaliki <malikitimi@gmail.com>
2023-08-24 09:59:11 +0100

    primitive vs object

---
+++ Hoisting/index.js
@@ -0,0 +1,31 @@
+//variable
+
+console.log(me)
+console.log(job)
+console.log(year)
+
+
+
+var me ="Timi"
+let job="student"
+const year=1999
+
+
+//functions
+
+console.log(addDecl(2,4))
+console.log(addExp(2,4))
+console.log(addArrrow(2,4))
+
+
+
+function addDecl(a,b){
+    return a+b
+}
+
+var addExp = function(a,b)
+{
+   return a+b
+}
+
+const addArrrow =( a,b) => a+b
