135db5351a89673ac71f11d5972b48829b3b528c
TimiMaliki <malikitimi@gmail.com>
2023-08-27 18:59:37 +0100

    objects

--- primitive vs Object/index.js
+++ primitive vs Object/index.js
@@ -1,7 +1,21 @@
 
+//primitive types
 let lastName = "Williams"
 let oldLastName = lastName;
 
 lastName = "Davis"
 
 console.log(lastName , oldLastName)
+
+//reference type
+
+const jessica = {
+    firstName:'jessica',
+    lastName:'william',
+    age: 27
+}
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Maliki';
+console.log('before marriage' , jessica)
+console.log('after marriage' , jessica)
