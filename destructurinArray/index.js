101740c28294187b55d21cdea9923ed829dbfc4e
TimiMaliki <malikitimi@gmail.com>
2023-08-28 01:23:34 +0100

    destructiing

---
+++ destructurinArray/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  
+  order: function(starterIndex , mainIndex){
+       return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  }
+}
+
+const arr = [2,3,4]
+const a = arr[0]
+const b = arr[1]
+const c = arr[2]
+
+// console.log(a,b,c)
+
+const [x,y,z] = arr;
+
+console.log(x,y,z);
+
+
+// let [main , ,secondary] = restaurant.categories
+// console.log(main , secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+
+//  [main , secondary] = [secondary , main]
+
+
+const [starter ,mainCourse] = restaurant.order(2,0)
+
+console.log(starter,mainCourse)
+
+
+const nested = [2,3 ,[5,6]];
+
+// const [i,,j] = nested
+
+// console.log(i,j)
+
+
+const [i,,[j,k]] = nested
+
+console.log(i,j,k)
