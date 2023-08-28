7b00561a9c2eae23d377b91f08b0ecb77a9be4e1
TimiMaliki <malikitimi@gmail.com>
2023-08-28 03:55:31 +0100

    destructiing

--- destructurinArray/index.js
+++ destructurinArray/index.js
@@ -1,5 +1,4 @@
-'use strict';
-
+"use strict";
 
 // const restaurant = {
 //   name: 'Classico Italiano',
@@ -7,7 +6,7 @@
 //   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
 //   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
 //   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  
+
 //   order: function(starterIndex , mainIndex){
 //        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
 //   }
@@ -24,7 +23,6 @@
 
 // console.log(x,y,z);
 
-
 // let [main , ,secondary] = restaurant.categories
 // console.log(main , secondary);
 
@@ -32,14 +30,11 @@
 // main = secondary;
 // secondary = temp;
 
-
 //  [main , secondary] = [secondary , main]
-
 
 // const [starter ,mainCourse] = restaurant.order(2,0)
 
 // console.log(starter,mainCourse)
-
 
 // const nested = [2,3 ,[5,6]];
 
@@ -47,98 +42,106 @@
 
 // // console.log(i,j)
 
-
 // const [i,,[j,k]] = nested
 
 // console.log(i,j,k)
 
-
-
 //desturing objects
 
 const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  
-    openingHours: {
-      thu: {
-        open: 12,
-        close: 22,
-      },
-      fri: {
-        open: 11,
-        close: 23,
-      },
-      sat: {
-        open: 0, // Open 24 hours
-        close: 24,
-      },
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
     },
-    order: function(starterIndex , mainIndex){
-        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
-   },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
-orderDelivery:function({mainIndex=1,starterIndex=1,time=`1:30`,address}){
-
+  orderDelivery: function ({
+    mainIndex = 1,
+    starterIndex = 1,
+    time = `1:30`,
+    address,
+  }) {
     console.log(`order received! ${this.starterMenu[starterIndex]}
-    , ${this.mainMenu[mainIndex]} will be delivered at ${address} ,exactly${time}... prompt`)
-    
+    , ${this.mainMenu[mainIndex]} will be delivered at ${address} ,exactly${time}... prompt`);
+  },
+};
 
-}
+restaurant.orderDelivery({
+  time: "22:00",
+  address: "mile 50",
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
-
-  };
-
-  restaurant.orderDelivery({
-    time:'22:00',
-    address:'mile 50',
-    mainIndex :2,
-    starterIndex:2
-
-  })
-
-
-  const {name, openingHours, mainMenu} = restaurant
+const { name, openingHours, mainMenu } = restaurant;
 // console.log(name, openingHours, mainMenu)
 
-const {name:restaurantName , openingHours:hours ,
-categories:tags } = restaurant 
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
 
 // console.log(restaurantName,tags,hours)
 
-const {menu = [], starterMenu:starters =[]} = restaurant
+const { menu = [], starterMenu: starters = [] } = restaurant;
 
 //Mutating variables
 
-let a = 111
-let b = 999
- 
-const obj = { a:23, b:7 , c:34};
+let a = 111;
+let b = 999;
 
-({a , b} = obj);
+const obj = { a: 23, b: 7, c: 34 };
 
-console.log(a,b);
+({ a, b } = obj);
+
+console.log(a, b);
 //nested objects
 
-const {fri:{open:o,close:c}} = openingHours 
-console.log(o,c)
-
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
 
 //spread operator
 
-const arr = [7,8,9];
-const badArr = [4,5,6,arr[0],arr[1],arr[2]]
+const arr = [7, 8, 9];
+const badArr = [4, 5, 6, arr[0], arr[1], arr[2]];
 
-console.log(badArr)
+console.log(badArr);
 
-const betterArr = [4,5,6, ...arr]
+const betterArr = [4, 5, 6, ...arr];
 
-console.log(...betterArr)
+console.log(...betterArr);
 
-const newArr = [...restaurant.mainMenu , 'ice-cream']
-console.log(newArr)
+const newArr = [...restaurant.mainMenu, "ice-cream"];
+console.log(newArr);
+
 //copy array
+const mainMenuCopy = [...restaurant.mainMenu];
 
+//join 2 arrays
+
+const menuCopy = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+console.log(menuCopy);
+
+//iterates: arrays, strings, maps,sets, Not objects
