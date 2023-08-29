1ee61e84d48a7fceb43e9e08f641f8f118367f02
TimiMaliki <malikitimi@gmail.com>
2023-08-29 18:41:58 +0100

    --loop-of--

---
+++ loop-of/index.js
@@ -0,0 +1,12 @@
+
+    //  const list = [1,2,3,4,5,6,6,7]
+
+    //  for (const num of list){
+    //     console.log(num)
+    //  }
+
+     const arr = [2,5,6]
+
+     for (const item of arr.entries()){
+         console.log([...arr.entries()])
+     }
