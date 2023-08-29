0928c8aae4026d70a45b4cd41b4482e104d27b89
TimiMaliki <malikitimi@gmail.com>
2023-08-29 19:18:26 +0100

    --loop-of--

--- loop-of/index.js
+++ loop-of/index.js
@@ -7,6 +7,7 @@
 
      const arr = [2,5,6]
 
-     for (const item of arr.entries()){
-         console.log(`${item[0] + 1}: yams and  ${arr[1] + 1}goats `)
+     for (const [i,el] of arr.entries()){
+        //  console.log(`${item[0] + 1}: yams and  ${arr[1] + 1}goats `)
+        console.log(`${i+ 1}:yams  ${el}`)
      }
