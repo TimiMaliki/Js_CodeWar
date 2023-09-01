187a05cfc3702d2e354593a464d83c7d9a0ebf6a
TimiMaliki <malikitimi@gmail.com>
2023-09-01 13:51:48 +0100

    maps

--- set/index.js
+++ set/index.js
@@ -1,1 +1,27 @@
- const setter = map.set
+const recordSet = new Set(["Book", "goceries", "junks", "books", "Book"]);
+
+
+console.log(recordSet.size);
+console.log(recordSet.has('Book'))
+console.log(recordSet.has("tv"))
+console.log(recordSet.add("tv"))
+console.log(recordSet.delete('books'))
+console.log(recordSet)
+
+
+// console.log(new Set('Timi'))
+
+for(const record of recordSet) console.log(record)
+
+
+const staff = ['waiter','chef','waiter','banker','foodie'
+,'techGuy']
+
+const staffUniue = [...new Set(staff)]
+
+console.log(staffUniue)
+
+const staffSize = new Set(['waiter','chef','waiter','banker','foodie'
+,'techGuy']).size
+
+console.log(staffSize)
